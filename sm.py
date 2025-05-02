07400b516f9211d67182390fffcd1dc2cbc5dc00
surya <suryapraveen2005@outlook.com>
2025-05-02 12:37:30 +0530

    new_changes

--- sm.py
+++ sm.py
@@ -1,10 +1,11 @@
 # 
 
-help("topics")
-help("modules packages")
-help("print")
+# help("topics")
+# help("modules packages")
+# help("print")
 
 import instaloader
 import os   
 ig = instaloader.Instaloader()  
-ig.login("username", "password") # login to your account
+# ig.login("username", "password") # login to your account
+ig.download_profile("itz_.sasi._xz", profile_pic_only=True) # download profile picture of the user
