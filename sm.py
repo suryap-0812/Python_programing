e263932aa433ce589d349e8fb19017a73d6569fc
surya <suryapraveen2005@outlook.com>
2025-05-02 13:44:33 +0530

    nice_work

--- sm.py
+++ sm.py
@@ -4,8 +4,13 @@
 # help("modules packages")
 # help("print")
 
-import instaloader
-import os   
-ig = instaloader.Instaloader()  
-# ig.login("username", "password") # login to your account
-ig.download_profile("itz_.sasi._xz", profile_pic_only=True) # download profile picture of the user
+# import instaloader
+# import os   
+# ig = instaloader.Instaloader()  
+# # ig.login("username", "password") # login to your account
+# ig.download_profile("itz_.sasi._xz", profile_pic_only=True) # download profile picture of the user
+
+
+import pywhatkit
+
+pywhatkit.sendwhatmsg_instantly("+917010791279", "Hello, this is a test message!") # send a message to the number at 3:00 PM
